0fd9958dd5cdbe7a816c1d8c0cfa5874235f0d7a
Mahmoudfathy1998 <mahmoud1611910@miuegypt.edu.eg>
2020-03-29 17:50:17 +0200

    7oda second commit

---
+++ app/src/main/java/com/example/carapp/Database/RequestApi.java
@@ -0,0 +1,179 @@
+package com.example.carapp.Database;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestApi {
+
+    private Context context;
+
+    public RequestApi(Context context) {
+        this.context = context;
+    }
+
+    public void selectApi(final VolleyCallBack callback, final String tableName, Map<String, String> conditions) {
+
+        Gson gson = new Gson();
+        final String con = gson.toJson(conditions);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "", new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                callback.onSuccess(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        callback.onError(error);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("table", tableName);
+                param.put("conditions", con);
+
+                return param;
+            }
+        };
+
+        VolleySingleton.getnInstance(context).addRequestQue(stringRequest);
+    }
+
+    public void reportApi(final VolleyCallBack callback, final String tableName, Map<String, String> conditions) {
+
+        Gson gson = new Gson();
+        final String con = gson.toJson(conditions);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "", new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                callback.onSuccess(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        callback.onError(error);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("table", tableName);
+                param.put("conditions", con);
+
+                return param;
+            }
+        };
+
+        VolleySingleton.getnInstance(context).addRequestQue(stringRequest);
+    }
+
+    public void insertApi(final VolleyCallBack callback, final String tableName, Map<String, String> data) {
+
+        Gson gson = new Gson();
+        final String insert_data = gson.toJson(data);
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,"", new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                callback.onSuccess(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        callback.onError(error);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("table", tableName);
+                param.put("data", insert_data);
+
+                return param;
+            }
+        };
+
+        VolleySingleton.getnInstance(context).addRequestQue(stringRequest);
+    }
+
+    public void updateApi(final String tableName, Map<String, String> data, Map<String, String> conditions) {}
+
+    public void deleteApi(final VolleyCallBack callback, final String tableName, Map<String, String> conditions) {
+
+        Gson gson = new Gson();
+        final String insert_data = gson.toJson(conditions);
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "", new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                callback.onSuccess(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        callback.onError(error);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("table", tableName);
+                param.put("data", insert_data);
+
+                return param;
+            }
+        };
+
+        VolleySingleton.getnInstance(context).addRequestQue(stringRequest);
+
+    }
+
+    /*public static void jsonToMap(String t) throws JSONException {
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        JSONObject jObject = new JSONObject(t);
+        Iterator<?> keys = jObject.keys();
+
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String value = jObject.getString(key);
+            map.put(key, value);
+        }
+
+        System.out.println("json : " + jObject);
+        System.out.println("map : " + map);
+    }*/
+
+}
+
+
---
+++ app/src/main/java/com/example/carapp/Database/VolleyCallBack.java
@@ -0,0 +1,6 @@
+package com.example.carapp.Database;
+
+public interface VolleyCallBack {
+    void onSuccess(String result);
+    void onError(Throwable throwable);
+}
