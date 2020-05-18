eb46847c44d40908e023456ff11d1669ca1482bc
Mahmoud Ahmed Fathy <62708550+Mahmoudfathy1998@users.noreply.github.com>
2020-05-18 17:28:48 +0200

    Delete SelectReportCallBack.java

--- app/src/main/java/com/example/carapp/Controller/CallBacks/SelectReportCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.example.carapp.Controller.CallBacks;
-
-import com.example.carapp.Entites.Report;
-
-public interface SelectReportCallBack {
-    void onSuccess(Report report);
-    void onfailure(String error);
-}
