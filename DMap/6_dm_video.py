8cefb73635c946f2cb53657322cf0cfe71490ffe
Mahmoud Ahmed Fathy <62708550+Mahmoudfathy1998@users.noreply.github.com>
2020-05-20 13:33:09 +0200

    Update 6_dm_video.py

--- DMap/6_dm_video.py
+++ DMap/6_dm_video.py
@@ -1,28 +1,3 @@
-# Copyright (C) 2019 Eugene Pomazov, <stereopi.com>, virt2real team
-#
-# This file is part of StereoPi tutorial scripts.
-#
-# StereoPi tutorial is free software: you can redistribute it 
-# and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3 of the 
-# License, or (at your option) any later version.
-#
-# StereoPi tutorial is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with StereoPi tutorial.  
-# If not, see <http://www.gnu.org/licenses/>.
-#
-# Most of this code is updated version of 3dberry.org project by virt2real
-# 
-# Thanks to Adrian and http://pyimagesearch.com, as there are lot of
-# code in this tutorial was taken from his lessons.
-# 
-
-
 from picamera import PiCamera
 import time
 import cv2
@@ -43,14 +18,13 @@ UR = 10
 SR = 14
 SPWS = 100
 
-# Camera settimgs
+
 cam_width = 1280
 cam_height = 480
 
-# Final image capture settings
 scale_ratio = 0.5
 
-# Camera resolution height must be dividable by 16, and width by 32
+# Note for us: Camera resolution height must be dividable by 16, and width by 32, so we use this equation.
 cam_width = int((cam_width+31)/32)*32
 cam_height = int((cam_height+15)/16)*16
 print ("Used camera resolution: "+str(cam_width)+" x "+str(cam_height))
@@ -61,7 +35,7 @@ img_height = int (cam_height * scale_ratio)
 capture = np.zeros((img_height, img_width, 4), dtype=np.uint8)
 print ("Scaled image resolution: "+str(img_width)+" x "+str(img_height))
 
-# Initialize the camera
+
 camera = PiCamera(stereo_mode='side-by-side',stereo_decimate=False)
 camera.resolution=(cam_width, cam_height)
 camera.framerate = 20
@@ -69,11 +43,11 @@ camera.hflip = False
 camera.vflip = True 
 
 
-# Implementing calibration data
+
 print('Read calibration data and rectifying stereo pair...')
 calibration = StereoCalibration(input_folder='calib_result')
 
-# Initialize interface windows
+
 cv2.namedWindow("Image")
 cv2.moveWindow("Image", 50,100)
 cv2.namedWindow("left")
@@ -150,7 +124,7 @@ for frame in camera.capture_continuous(capture, format="bgra", use_video_port=Tr
             else:
                 print("speed up")
                 break
-    # show the frame
+    
     cv2.imshow("left", imgLeft)
     cv2.imshow("right", imgRight)    
 
